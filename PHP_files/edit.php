5d145a85228fa1ecfb1b71d3d351fdf0e8e94f5a
HarshDhiman001 <harsh220000@gmail.com>
2023-03-29 16:13:14 +0530

    updated

--- PHP_files/edit.php
+++ PHP_files/edit.php
@@ -54,17 +54,15 @@ if($_SERVER['REQUEST_METHOD']=='POST')
 $id=$_GET['id'];
 $sql = "SELECT * FROM `data` WHERE id=$id";
 $result=mysqli_query($con,$sql);
+while($row=mysqli_fetch_assoc($result))
 {
-    while($row=mysqli_fetch_assoc($result))
-    {
-        $id = $row['id'];
-        $name = $row['name'];
-        $email = $row['email'];
-        $contact = $row['contact'];
-        $address = $row['address'];
-        $class = $row['class'];
-        $gender = $row['gender'];
-    }
+$id = $row['id'];
+$name = $row['name'];
+$email = $row['email'];
+$contact = $row['contact'];
+$address = $row['address'];
+$class = $row['class'];
+$gender = $row['gender'];
 }
 ?>
 <!doctype html>
@@ -83,13 +81,7 @@ $result=mysqli_query($con,$sql);
   <body>
 
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-</html>
+
 <form  method="post" action="" class="stdform">
         <div class="form-group">
           <label for="exampleInputName">Name</label>
@@ -144,5 +136,10 @@ $result=mysqli_query($con,$sql);
   </tbody>
 </table>
 </div>
-</body>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
 </html>
